93d19b526349a887e3b2d7abab0487e51079505f
kaufast <kaufast@gmail.com>
2025-10-02 15:45:05 +0200

    Fix TypeScript error in RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata = {
     description: 'Next Js template'
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en" className={`${openSans.variable} ${poppins.variable}`}>
             <body>{children}</body>
